7f0aa16193bfdc32b4ac036a21e330bd4bfce676
Felipe <ftiagomelo89@gmail.com>
2022-01-08 08:34:16 -0300

    Adicionando objetos de representacao de erros e tratamento de excecoes com um exception controller advice.

---
+++ src/main/java/com/example/felipe/expensemanager/api/exceptionhandler/ErrorType.java
@@ -0,0 +1,20 @@
+package com.example.felipe.expensemanager.api.exceptionhandler;
+
+import lombok.Getter;
+
+@Getter
+public enum ErrorType {
+
+    INVALID_DATA( "Informação inválida"),
+    INVALID_PARAMETER( "Parâmetro inválido"),
+    ILLEGIBLE_REQUEST( "Requisição ilegível"),
+    RESOURCE_NOT_FOUND( "Recurso não encontrado"),
+    BUSINESS_ERROR( "Business rule violation"),
+    SYSTEM_ERROR( "System error");
+
+    private final String title;
+
+    ErrorType(String title){
+        this.title = title;
+    }
+}
